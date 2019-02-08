51dc3a248e7a08072a78f54c03d7031c5557002a
Adrian Minde <Adrian_Minde@live.de>
2019-02-08 17:21:35 +0100

    Improve setup and update process

--- lib/framework/bundle/openmuc-driver-modbus.sh
+++ lib/framework/bundle/openmuc-driver-modbus.sh
@@ -12,11 +12,8 @@ install() {
     remove_bundle "openmuc-$SERVICE-$ID"
     install_bundle "$PROJECT" "openmuc-$SERVICE-$ID" "$OPENMUC_VERSION"
   fi
-  remove_lib "device/$ID"
-  install_lib core "device/$ID"
 }
 
 remove() {
   remove_bundle "openmuc-$SERVICE-$ID"
-  remove_lib "device/$ID"
 }
