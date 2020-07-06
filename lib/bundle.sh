1ae2b9511ae3b8d784d1a2f725ccc238b170b031
Adrian Minde <Adrian_Minde@live.de>
2020-07-06 11:25:38 +0200

    Fix updated bundle installation/removal script

--- lib/bundle.sh
+++ lib/bundle.sh
@@ -8,35 +8,31 @@ bundles() {
   service="driver"
 
   for bundle in "${@:2}"; do
-    case "$2" in
+    case "$bundle" in
       -a | --app)
         service="app"
-        continue
         ;;
       -d | --driver)
         service="driver"
-        continue
         ;;
       -l | --logger | --datalogger)
         service="datalogger"
-        continue
         ;;
       -s | --server)
         service="server"
-        continue
         ;;
       *)
         bundle "$1" "$service" "$bundle"
         ;;
     esac
   done
-  php "$EMONMUC_DIR"/lib/www/reload.php
 }
 
 bundle() {
   bundle="openmuc-$2-$3.gradle"
 
-  if ! [ -f "$BUNDLE_CONFIGS/$bundle" ]; then
+  if ! [ -d "$BUNDLE_CONFIGS" ]; then mkdir -p "$BUNDLE_CONFIGS"; fi
+  if ! [ -f "$BUNDLE_DIR/$bundle" ]; then
 	echo "Unable to $1 unknown $2: $3"
 	return 1
   fi
